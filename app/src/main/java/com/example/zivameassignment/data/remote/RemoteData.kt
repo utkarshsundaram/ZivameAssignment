067aba7bfcc65e4255db3d5d714340e531e17c65
Utkarsh Sundaram <ustheleo@gmail.com>
2021-09-28 21:43:37 +0530

    Network layer added

---
+++ app/src/main/java/com/example/zivameassignment/data/remote/RemoteData.kt
@@ -0,0 +1,6 @@
+package com.example.zivameassignment.data.remote
+
+/**
+* Created by Utkarsh Sundaram on 28-09-2021.
+*/class RemoteData {
+}
---
+++ app/src/main/java/com/example/zivameassignment/data/remote/service/CartService.kt
@@ -0,0 +1,6 @@
+package com.example.zivameassignment.data.remote.service
+
+/**
+* Created by Utkarsh Sundaram on 28-09-2021.
+*/class CartService {
+}
